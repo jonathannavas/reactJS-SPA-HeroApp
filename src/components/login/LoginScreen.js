4601d0bf43f0e51358881462825239f1bfbfd883
Jonathan Navas <jonathan.navas9@hotmail.com>
2021-10-06 20:06:19 -0500

    Routes auth

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const LoginScreen = ({ history }) => {
 
-    const handleLogin = () => {
-        history.replace('/');
-    }
+    const {dispatch} = useContext(AuthContext);
 
+    const handleLogin = () => {
+
+        const lastPath = localStorage.getItem('lastPath') || '/';
+        
+        const payload = {
+            name: 'Jonathan'
+        }
+        dispatch({
+            type: types.login,
+            payload
+        });
+        history.replace(lastPath);
+    }
+    
     return (
         <div className="container mt-5">
             
